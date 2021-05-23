6f39f4786f119ae9422da1d79bc9500f528e7090
ShruthiHanuGowda <shruthi.anugowda@gmail.com>
2021-05-23 06:22:51 +0200

    tiktok-backend-server commit

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    url: 'https://player.vimeo.com/external/474196884.sd.mp4?s=ccda82b20358a5037fa6329979727eb3ea5723e2&profile_id=165&oauth2_token_id=57447761',
+    channel: 'shruthi',
+    description: 'wow',
+    song: '99 problems but react aint one',
+    likes: '123',
+    messages: '456',
+    shares: '789',
+  },
+
+  {
+    url: 'https://player.vimeo.com/external/464571806.sd.mp4?s=ea84c1c3cdd312fc07aa6bcf90aa09a986c261ac&profile_id=165&oauth2_token_id=57447761',
+    channel: 'shruthi',
+    description: 'wow',
+    song: '99 problems but react aint one',
+    likes: '778',
+    messages: '56',
+    shares: '87',
+  },
+];
+
+export default data;
---
+++ server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Data from './data.js';
+import Videos from './dbModel.js';
+
+//app config
+const app = express();
+const port = 9000;
+
+//middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'),
+    res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+//DB config
+const connection_url =
+  'mongodb+srv://admin:Tp2KwUAYRUFa1VwS@cluster0.l1bsg.mongodb.net/tiktok?retryWrites=true&w=majority';
+
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+//api endpoints
+app.get('/', (req, res) => res.status(200).send('hello'));
+app.get('/v1/posts', (req, res) => res.status(200).send(Data));
+
+app.get('/v2/posts', (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post('/v2/posts', (req, res) => {
+  //POST request to add data to the database
+  //it will let us to add a video document to videos collection
+  const dbVideos = req.body;
+
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+//listen
+app.listen(port, () => console.log(`listening: ${port}`));
